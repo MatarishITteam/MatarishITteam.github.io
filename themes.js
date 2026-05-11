be63a390618ede629463e61a9d3a26fed39c2cc1
Yusuf Mohamed Mahmoud Mohamed Mansour <yusuf.2122069@gmail.com>
2026-05-11 21:19:26 +0300

    Add files via upload

--- themes.js
+++ themes.js
@@ -1,18 +1,20 @@
 const Button = document.getElementById("themeToggle");
 
-if(localStorage.getItem("theme") === "dark"){
+if (localStorage.getItem("theme") === "dark") {
     document.body.classList.add("dark");
-    Button.textContent = "☀️";
+    Button.className = "fa-regular fa-sun";
+} else {
+    Button.className = "fa-regular fa-moon";
 }
 
-    Button.addEventListener("click", () => {
+Button.addEventListener("click", () => {
     document.body.classList.toggle("dark");
 
-    if(document.body.classList.contains("dark")){
+    if (document.body.classList.contains("dark")) {
         localStorage.setItem("theme", "dark");
-        Button.textContent = "☀️";
-    }else{
+        Button.className = "fa-regular fa-sun";
+    } else {
         localStorage.setItem("theme", "light");
-        Button.textContent = "🌙";
+        Button.className = "fa-regular fa-moon";
     }
 });
